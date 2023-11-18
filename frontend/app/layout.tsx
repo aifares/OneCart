07315ce1f69f6452c72b159fe935997f4133b19b
aifares <aifares@buffalo.edu>
2023-11-18 02:06:52 -0500

    Init Commit

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
-        {children}
+        <section className="contentWrapper">{children}</section>
       </body>
     </html>
   );
--- frontend/app/yourcart/page.tsx
+++ frontend/app/yourcart/page.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 
-
-
-
-
-
 const Page = () => {
-  return <div>yourCart</div>;
+  return (
+    <section className="">
+      <h1>yourCart</h1>
+      <div className="columns-2xs">
+        <div
+          className="w-fit p-5
+         bg-slate-50 rounded-xl shadow-lg items-center transform transition duration-500 hover:scale-105 hover:border hover:border-sky-500 overflow-hidden"
+        >
+          <div> COMPANY LOGO </div>
+        </div>
+        <div className="w-full p-5  bg-slate-50 rounded-xl shadow-lg items-center transform transition duration-500 hover:scale-105 hover:border hover:border-sky-500 overflow-hidden">
+          <p>Your Cart </p>
+          <p>Number Of Items: </p>
+          <p>Total:</p>
+          <button>View Cart</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Page;
